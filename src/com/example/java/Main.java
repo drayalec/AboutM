44b5ebd09c4d19fb8efe21380e7b6294be3982aa
drayalec <31783033+drayalec@users.noreply.github.com>
2018-01-21 23:20:30 -0600

    Initial commit

---
+++ src/com/example/java/Main.java
@@ -0,0 +1,11 @@
+package com.example.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hi! My name is Alec!");
+        System.out.println("I enjoy programming and cant wait to learn more");
+        System.out.println("My first time writing code was when I was 13");
+        System.out.println("But this will be my first time writing in Java!");
+    }
+}
